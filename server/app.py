3caa8906fc312a590338a5a06247d311de86e8ea
Zack Zellto <zacharyzellto@gmail.com>
2023-04-15 13:56:35 -0600

    SwaggerUI finally working, need to add endpoints.

--- server/app.py
+++ server/app.py
@@ -1,14 +1,23 @@
-from flask import render_template
-import connexion
+from flask import Flask, Blueprint
+from flask_swagger_ui import get_swaggerui_blueprint
 
-app = connexion.App(__name__, specification_dir="./")
-app.add_api("swagger.yml")
+app = Flask(__name__)
 
+# Create the Swagger UI blueprint
+# URL for exposing Swagger UI (without trailing '/')
+SWAGGER_URL = '/swagger'
 
-@app.route("/")
-def home():
-    return render_template("home.html")
+# URL for exposing OpenAPI spec (without trailing '/')
+API_URL = '/static/swagger.json'
+SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Fmly Landing Page API"
+    }
+)
 
+app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+if __name__ == '__main__':
+    app.run()
